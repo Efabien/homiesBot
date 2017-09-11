10802fc73b4a4c4cf537a77bb3644a89ef40d584
EnoneDev <Enone@protonmail.com>
2017-09-11 21:55:46 +0300

    beta version

--- modules/handlers/post-back-handler.js
+++ modules/handlers/post-back-handler.js
@@ -1,25 +1,9 @@
-const footFunctionality = require('./functionality/foot');
 const template = require('../../template');
 
-module.exports = (fb, sender, data, res) => {
-  const commande = data.payload.split('_');
-  const type = commande[0];
-  const action = commande[1];
-  const league = footFunctionality.retrieveLeague(commande[2]);
-  const subAction = commande[3];
-  const id = commande[4];
+module.exports = (fb, sender, data, payloadHandeler, res) => {
+  
+  payloadHandeler.receive(data.payload);
+  const commande = payloadHandeler.getCommande();
 
-  if(type === 'start') {
-    fb.sendText(sender, template.welcomeMessage);
-  }
-  if (type === 'foot') {
-     if (action === 'getNews') footFunctionality.trigger(fb, sender);
-     if (action === 'help') fb.sendText(sender, template.helpMessage);
-     if (action === 'result') {
-      if (subAction === 'latestMatch') footFunctionality.latestMatch(fb, sender);
-      if (subAction === 'detail') footFunctionality.matchResult(fb, sender, league, id);
-      if (subAction === 'next') footFunctionality.latestMatchFromId(fb, sender, league, id, 4);
-     }
-  }
   res.sendStatus(200);
 }
--- modules/handlers/quick-reply-handler.js
+++ modules/handlers/quick-reply-handler.js
@@ -1,10 +1,4 @@
-const footFunctionality = require('./functionality/foot');
-
-module.exports = (fb, sender, data, res) => {
-	const type = data.payload.split('_')[0];
-	if (type === 'foot') {
-		const ref = data.payload.split('_')[1];
-		footFunctionality.nextNew(fb, sender, ref)
-		res.sendStatus(200);
-	}
+module.exports = (fb, sender, data, payloadHandeler, res) => {
+	const payloadHandeler.receive(data.payload);
+  const commande = payloadHandeler.getCommande();
 }
