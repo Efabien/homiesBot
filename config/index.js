62c98cb9a6191d70a3c5f3a89325c3bcaa326c48
EnoneDev <Enone@protonmail.com>
2017-12-11 23:00:06 +0300

    setting env vars

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 module.exports = {
-	token: process.env.FB_TOKEN_HOMIES,
+	verifyToken: process.env.FB_VERIFY_TOKEN,
+	token: process.env.FB_HOMIES_APP_TOKEN,
 	graphAPI: 'https://graph.facebook.com/v2.6',
 	messengerProfile: 'https://graph.facebook.com/v2.6/me/messenger_profile',
 	messengerAPI: 'https://graph.facebook.com/v2.6/me/messages',
--- middlewares/fb-verification.js
+++ middlewares/fb-verification.js
@@ -1,7 +1,7 @@
 const config = require('../config');
 
 module.exports = function (req, res) {
-	if (req.query['hub.verify_token'] === config.token) {
+	if (req.query['hub.verify_token'] === config.verifyToken) {
 		res.send(req.query['hub.challenge']);
 	} else {
 		res.send('Error, wrong token');
