24b8928766012e810bbc686e095d59d9674057af
EnoneDev <Enone@protonmail.com>
2017-12-11 22:42:51 +0300

    dependencies

--- config/index.js
+++ config/index.js
@@ -1,10 +1,9 @@
 module.exports = {
-	token: process.env.FB_TOKEN_NEW_BORN,
+	token: process.env.FB_TOKEN_HOMIES,
 	graphAPI: 'https://graph.facebook.com/v2.6',
 	messengerProfile: 'https://graph.facebook.com/v2.6/me/messenger_profile',
 	messengerAPI: 'https://graph.facebook.com/v2.6/me/messages',
-	restifeoBaseUrl: process.env.RESTIFEO,
-	restifeoAuthToken : process.env.RESTIFEO_AUTH_TOKEN,
+	homiesApiBaseUrl: process.env.HOMIES_API,
 	accessAPI: process.env.ACCESS_API,
 	payloadMap: ['type', 'action', 'subaction', 'param', 'id'],
 	payloadSeparator: '_'
