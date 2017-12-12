75c90de2af82bfb5af05061dde0569d5a2a9caed
EnoneDev <Enone@protonmail.com>
2017-12-12 12:37:06 +0300

    welcome message

--- template/index.js
+++ template/index.js
@@ -1,4 +1,5 @@
 module.exports = {
-	welcomeMessage: `Bonjour {{userName}}, bienvenue.`,
+	welcomeMessage: `Hey {{userName}}, welcome onboard!. #We all love sharing with our homies when we are about to skate, right? 
+#I will help you doing that. There is more to be discoverd!`,
 	helpMessage: `help`
 }
